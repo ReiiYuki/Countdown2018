722294ac5bb7dac418a208d1ed956a6b383b6164
Voraton Lertrattanapaisal <voraton.l@ku.th>
2017-12-31 22:34:21 +0700

    Get Data But not Update

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -2,16 +2,37 @@ import MsgItem from './MsgItem'
 import React from 'react'
 import styled from 'styled-components'
 
-const ChatBox = styled.section`
+const ChatContainer = styled.section`
     overflow: scroll;
     height: 50vh;
     margin: 10px;
 `
 
-export default (props) => (
-  <ChatBox className='hero is-medium is-primary'>
-    <div className='hero-head'>
-      {props.get().map((msgItem) => <MsgItem user={msgItem.user} msg={msgItem.msg} />)}
-    </div>
-  </ChatBox>
-)
+export default class ChatBox extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.state = {
+      msgs: []
+    }
+  }
+
+  componentDidMount () {
+    const self = this
+    this.props.get().then(data => {
+      self.setState({
+        msgs: data
+      })
+    })
+  }
+
+  render () {
+    const { msgs } = this.state
+    return (
+      <ChatContainer className='hero is-medium is-primary'>
+        <div className='hero-head'>
+          {msgs.map((msgItem) => <MsgItem user={msgItem.user} msg={msgItem.msg} />)}
+        </div>
+      </ChatContainer>
+    )
+  }
+}
--- src/modules/App.js
+++ src/modules/App.js
@@ -2,5 +2,4 @@ import FirebaseUtil from './FirebaseUtil'
 import Page from '../components/Page'
 import React from 'react'
 
-console.log(FirebaseUtil.getMsg())
 export default () => <Page fire={FirebaseUtil} />
