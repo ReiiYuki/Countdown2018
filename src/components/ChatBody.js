b900998f1b712f7b90ce9ed10b742e90f5aae9a8
Voraton Lertrattanapaisal <voraton.l@ku.th>
2017-12-31 21:16:38 +0700

    Input Box

--- src/components/ChatBody.js
+++ src/components/ChatBody.js
@@ -1,10 +1,12 @@
 import ChatBox from './ChatBox'
+import ChatInput from './ChatInput'
 import React from 'react'
 
 export default () => (
   <section className='section'>
     <div className='container'>
       <ChatBox />
+      <ChatInput />
     </div>
   </section>
 )
